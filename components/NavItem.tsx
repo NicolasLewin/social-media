0135ec0d821d230a8eccd4235df94644f1ff53e0
NicolasLewin <nicolas.lewin@outlook.com>
2024-12-04 23:50:08 +0100

    updated NavItem to better handle href

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -7,14 +7,24 @@ interface NavItemProps {
   href?: string;
 }
 
-export default function NavItem({ icon: Icon, label, href = "" }: NavItemProps) {
-  const Component = href ? Link : "div";
-  return (
-    <Component href={href} className="w-full">
-      <button className="flex items-center gap-4 p-3 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full">
-        <Icon className="h-6 w-6 text-black dark:text-white" />
-        <span className="hidden md:inline text-xl text-black dark:text-white">{label}</span>
-      </button>
-    </Component>
+//TODO: remove the optional href later
+
+export default function NavItem({ icon: Icon, label, href }: NavItemProps) {
+  const content = (
+    <button className="flex w-full items-center gap-4 p-3 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full">
+      <Icon className="h-6 w-6 text-black dark:text-white" />
+      <span className="hidden md:inline text-xl text-black dark:text-white">{label}</span>
+    </button>
   );
-};
+
+  if (href) {
+    return (
+      <Link href={href} className="w-full">
+        {content}
+      </Link>
+    );
+  }
+
+  return <div className="w-full">{content}</div>;
+
+}
