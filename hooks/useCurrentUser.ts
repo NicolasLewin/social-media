941af0ce01a8bd20e1dc1eab0173061ed9876783
NicolasLewin <nicolas.lewin@outlook.com>
2025-01-08 21:30:34 +0100

    updated useCurrentUser

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,7 +1,23 @@
 import { useSession } from "next-auth/react";
+import { User } from "@prisma/client";
+
+interface ExtendedUser extends Partial<User> {
+  id: string;
+  email?: string | null;
+  name?: string | null;
+  image?: string | null;
+}
+
+interface ExtendedSession {
+  user?: ExtendedUser;
+  expires: string;
+}
 
 export const useCurrentUser = () => {
-  const { data: session, status } = useSession();
+  const { data: session, status } = useSession() as { 
+    data: ExtendedSession | null;
+    status: "loading" | "authenticated" | "unauthenticated";
+  };
   
   return {
     user: session?.user,
